f6fa22fe7d5d594f230b6136eafacaf12c44f1a4
amykglen <amy.glen@phenomehealth.org>
2025-04-04 14:25:14 -0700

    Add domain/range documentation; some refactoring

--- main.py
+++ main.py
@@ -171,14 +171,16 @@ def get_app_info() -> list:
 
     info = [
     html.Div(
-        style={"padding": "30px", "max-width": "800px", "margin": "0 auto"},  # Increased padding to 30px
+        style={"padding": "30px", "max-width": "800px", "margin": "0 auto", "margin-bottom": "20px", "overflowY": "auto", "height": "calc(100vh - 96px)"},
         children=[
             html.H3("About this app"),
             html.P([
                 "This application is designed to visualize and explore the relationships between "
                 "categories (i.e. node types) and predicates (i.e., edge types) within the ",
                 html.A("Biolink Model", href="https://github.com/biolink/biolink-model", target="_blank"),
-                "."
+                ", a schema for biomedical knowledge graphs developed by the ",
+                html.A("NCATS Biomedical Data Translator", href="https://ncats.nih.gov/research/research-activities/translator", target="_blank"),
+                " consortium."
             ]),
             html.H4("Using the tabs:"),
             html.P("""
@@ -224,10 +226,124 @@ def get_app_info() -> list:
                 Note that if you search for an item that is a mixin but 'Show mixins?' is not selected, the app
                 will override 'Show mixins?' and set it to True.
             """),
-        ]
+            html.H4("Domain and Range Filters (Predicates Tab):"),
+            html.P("""
+                On the 'Predicates' tab, you'll find dropdown menus labeled 'Filter by Domain' and 'Filter by Range'.
+                These filters allow you to narrow down the displayed predicates based on the types of categories
+                that are involved in the relationship.
+            """),
+            html.P("""
+                You can use these filters independently or together to explore specific types of relationships
+                between different categories in the Biolink Model. For instance, you could filter for predicates
+                that have 'Disease' as a domain and 'Symptom' as a range to see predicates that can be used to
+                link diseases to their symptoms.
+            """),
+            html.H5("Domain Filter:"),
+            html.P([
+                "The 'Domain' of a predicate refers to the category of entity that is the ",
+                html.B("subject"),
+                " or source of the relationship. For example, for the predicate 'has_phenotype', the domain might be "
+                "'Disease' because a disease can have a phenotype. "
+            ]),
+            html.P(["When you select one or more categories in the ",
+                "'Filter by Domain' dropdown, the graph will only show predicates where the subject of the ",
+                "relationship belongs to one of the selected categories (or their ancestors ",
+                "in the category hierarchy). This makes this a convenient way of seeing all predicates that could ",
+                "describe an edge in a knowledge graph that connects a Disease to some other node, for instance."
+            ]),
+            html.H5("Range Filter:"),
+            html.P([
+                "The 'Range' of a predicate refers to the category of entity that is the ",
+                html.B("object"),
+                " or target of the relationship. For example, for the predicate 'has_phenotype', the range might be ",
+                "'Phenotype' because a disease can have a phenotype. "
+            ]),
+            html.P(["When you select one or more categories in the ",
+                    "'Filter by Range' dropdown, the graph will only show predicates where the object of the ",
+                    "relationship belongs to one of the selected categories (or their ancestors ",
+                    "in the category hierarchy). This makes this a convenient way of seeing all predicates that could ",
+                    "describe an edge in a knowledge graph that connects some node to a Phenotype, for instance."
+                    ]),
+            html.H4("Creators"),
+            html.P(["This application was developed by ",
+                    html.A("Amy Glen", href="https://github.com/amykglen", target="_blank"),
+                    " at ",
+                    html.A("Phenome Health", href="https://www.phenomehealth.org", target="_blank"),
+                    "."])
+        ],
     )
 ]
     return info
+
+def get_filter_divs_preds():
+    return html.Div([
+        get_search_filter("node-search-preds", all_predicates),
+        get_mixin_filter("include-mixins-preds", show_by_default=True),
+        html.Div([
+            html.Label("Filter by Domain (hierarchical):"),
+            dcc.Dropdown(
+                id="domain-filter",
+                options=[{"label": d, "value": d} for d in domains],
+                multi=True,
+                placeholder="Select one or more domains..."
+            )
+        ], style={"width": "20%", "display": "inline-block", "padding": "0 1%"}),
+        html.Div([
+            html.Label("Filter by Range (hierarchical):"),
+            dcc.Dropdown(
+                id="range-filter",
+                options=[{"label": r, "value": r} for r in ranges],
+                multi=True,
+                placeholder="Select one or more ranges..."
+            )
+        ], style={"width": "20%", "display": "inline-block", "padding": "0 1%"})
+    ], style=filters_wrapper_style)
+
+def get_filter_divs_cats():
+    return html.Div([
+        get_search_filter("node-search-cats", all_categories),
+        get_mixin_filter("include-mixins-cats", show_by_default=False)
+    ], style=filters_wrapper_style)
+
+
+def get_app_layout():
+    return html.Div(
+        id="app-container",
+        children=[
+            dcc.Tabs([
+                dcc.Tab(label="Categories", children=[
+                    html.Div(
+                        style={"display": "flex", "flex-direction": "column", "height": "calc(100vh - 100px)"}, # Adjust 50px for tabs height
+                        children=[
+                            get_filter_divs_cats(),
+                            cyto.Cytoscape(
+                                id="cytoscape-dag-cats",
+                                elements=elements_categories,
+                                layout=layout_settings,
+                                style={"width": "100%", "height": "100%"}, # Set height to 100% of parent
+                                stylesheet=main_styling
+                            ),
+                            html.Div(id="node-info-cats", style=node_info_div_style)
+                        ])
+                ]),
+                dcc.Tab(label="Predicates", children=[
+                    html.Div(
+                        style={"display": "flex", "flex-direction": "column", "height": "calc(100vh - 100px)"}, # Adjust 50px for tabs height
+                        children=[
+                            get_filter_divs_preds(),
+                            cyto.Cytoscape(
+                                id="cytoscape-dag-preds",
+                                elements=elements_predicates,
+                                layout=layout_settings,
+                                style={"width": "100%", "height": "100%"}, # Set height to 100% of parent
+                                stylesheet=main_styling
+                            ),
+                            html.Div(id="node-info-preds", style=node_info_div_style)
+                        ])
+                ]),
+                dcc.Tab(label="Info", children=get_app_info())
+            ]),
+    ])
 
 
 # ----------------------------------------------- Style variables -------------------------------------------------- #
@@ -321,75 +437,11 @@ all_predicates = sorted(set(bd.predicate_dag.nodes()))
 
 
 # Initialize Dash app
-app = Dash(__name__, title="Biolink Viz")
-
-filters_div_preds = html.Div([
-    get_search_filter("node-search-preds", all_predicates),
-    get_mixin_filter("include-mixins-preds", show_by_default=True),
-    html.Div([
-        html.Label("Filter by Domain (hierarchical):"),
-        dcc.Dropdown(
-            id="domain-filter",
-            options=[{"label": d, "value": d} for d in domains],
-            multi=True,
-            placeholder="Select one or more domains..."
-        )
-    ], style={"width": "20%", "display": "inline-block", "padding": "0 1%"}),
-    html.Div([
-        html.Label("Filter by Range (hierarchical):"),
-        dcc.Dropdown(
-            id="range-filter",
-            options=[{"label": r, "value": r} for r in ranges],
-            multi=True,
-            placeholder="Select one or more ranges..."
-        )
-    ], style={"width": "20%", "display": "inline-block", "padding": "0 1%"})
-], style=filters_wrapper_style)
-
-filters_div_cats = html.Div([
-    get_search_filter("node-search-cats", all_categories),
-    get_mixin_filter("include-mixins-cats", show_by_default=False)
-], style=filters_wrapper_style)
+app = Dash(__name__, title="Biolink Explorer")
 
 
 # Define our tabs
-app.layout = html.Div(
-    id="app-container",
-    children=[
-        dcc.Tabs([
-            dcc.Tab(label="Categories", children=[
-                html.Div(
-                    style={"display": "flex", "flex-direction": "column", "height": "calc(100vh - 100px)"}, # Adjust 50px for tabs height
-                    children=[
-                        filters_div_cats,
-                        cyto.Cytoscape(
-                            id="cytoscape-dag-cats",
-                            elements=elements_categories,
-                            layout=layout_settings,
-                            style={"width": "100%", "height": "100%"}, # Set height to 100% of parent
-                            stylesheet=main_styling
-                        ),
-                        html.Div(id="node-info-cats", style=node_info_div_style)
-                    ])
-            ]),
-            dcc.Tab(label="Predicates", children=[
-                html.Div(
-                    style={"display": "flex", "flex-direction": "column", "height": "calc(100vh - 100px)"}, # Adjust 50px for tabs height
-                    children=[
-                        filters_div_preds,
-                        cyto.Cytoscape(
-                            id="cytoscape-dag-preds",
-                            elements=elements_predicates,
-                            layout=layout_settings,
-                            style={"width": "100%", "height": "100%"}, # Set height to 100% of parent
-                            stylesheet=main_styling
-                        ),
-                        html.Div(id="node-info-preds", style=node_info_div_style)
-                    ])
-            ]),
-            dcc.Tab(label="Info", children=get_app_info())
-        ]),
-])
+app.layout = get_app_layout()
 
 
 # Callbacks to filter graph elements based on dropdown/other selections
